5dc95d7b5153748d4946a0eb1f47a913747b3dc6
yVitorXD <vitorxdxx@gmail.com>
2023-09-02 20:28:58 -0300

    Model Cliente

---
+++ src/br/com/model/Clientes.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.model;
+
+/**
+ *
+ * @author Vitor
+ */
+public class Clientes {
+    
+    //Atributos
+    
+    private int id;
+    
+    private String nome;
+    
+    private String rg;
+    
+    private String cpf;
+    
+    private String email;
+    
+    private String telefone;
+    
+    private String celular;
+    
+    private String cep;
+    
+    private String endereco;
+    
+    private int numero;
+    
+    private String complemento;
+    
+    private String bairro;
+    
+    private String cidade;
+    
+    private String uf;
+    
+    
+    
+    
+}
